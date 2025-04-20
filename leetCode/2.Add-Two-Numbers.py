c39998ef188f30b37f61476253c3b1139135b776
David Agudelo <agudelo.david@hotmail.com>
2025-04-20 14:53:40 -0400

    array -> ListNode

--- leetCode/2.Add-Two-Numbers.py
+++ leetCode/2.Add-Two-Numbers.py
@@ -1,35 +1,36 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1:ListNode, l2:ListNode) -> ListNode:
-        for i in range(max(len(l1),len(l2))): # hago un loop del maximo de los dos arrays
-            if i >= len(l1):
-                l1.append(0)
-            if i >= len(l2):
-                l2.append(0)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) ->ListNode:
+        dummy = ListNode()  # Initialize dummy head
+        cur = dummy         # Initialize current
+        carry = 0           # Initialize carry
 
-            # si la suma de L1[i] + L2[i] es mayor a 9, entonces guardo el carry y lo sumo al siguiente elemento
-            if l1[i] + l2[i] > 9:
-                
-                l1[i+1] += 1
-                l1[i] = (l1[i] + l2[i]) % 10
-            else:
-                l3[i] = l1[i] + l2[i]
+        while l1 or l2 or carry:
+            v1 =l1.val if l1 else 0
+            v2 =l2.val if l2 else 0
             
-    
-        
-          
-        return 
-        
-#l1 = [2,4,3]
-#l2 = [5,6,4]
+            #new digits
+            
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+            
+            #update ptrs
+            cur = cur.next  
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
 
-l1 = [9,9,9,9,9,9,9]
-l2 = [9,9,9,9]
+# Test
+l1 = ListNode([9,9,9,9,9,9,9])
+l2 = ListNode([9,9,9,9])
+result = Solution().addTwoNumbers(l1, l2)
+print(result)
 
-Solution().addTwoNumbers(l1, l2)
-
-         
+# Output: [8,9,9,9,0,0,0,1]
