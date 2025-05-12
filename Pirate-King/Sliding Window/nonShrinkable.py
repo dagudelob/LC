8688f3633573f6e38d3dfef1cdedc989ea84cf45
Dagudelob <agudelo.david@hotmail.com>
2025-05-12 12:39:25 -0400

    Sliding Window

---
+++ Pirate-King/Sliding Window/nonShrinkable.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+def lengthOfLongestSubstringWithoutRepeating(s: str) -> int:
+    """
+    Given a string s, find the length of the longest substring without repeating characters.
+    
+    :param s: the string
+    :type s: str
+    :return: the length of the longest substring without repeating characters
+    :rtype: int
+    """
+    
+    map = defaultdict(int) # track count of each character
+    j = ans = 0
+    for i in range(len(s)):
+        map[s[i]] += 1 # update state using s[i]
+        while map[s[i]] > 1:
+            map[s[j]] -= 1 
+            j += 1 # shrink the left edge using s[j]
+        ans = max(ans, i - j + 1) # longest window so far
+    return ans # longest window
+
+def lengthOfLongestSubstringKDistinct(s: str, k: int) -> int:
+    """
+    Given a string s and a positive integer k, find the length of the longest substring of s that contains at most k distinct characters.
+    
+    :param s: the string
+    :type s: str
+    :param k: the number of distinct characters
+    :type k: int
+    :return: the length of the longest substring of s that contains at most k distinct characters
+    :rtype: int
+    """
+    map = defaultdict(int) # track count of each character
+    j = cnt = ans = 0
+    for i in range(len(s)):
+        c = s[i]
+        map[c] += 1
+        if map[c] == 1: cnt += 1 # update state using s[i]
+        while cnt > k:
+            c = s[j]
+            map[c] -= 1
+            if map[c] == 0: cnt -= 1 # update state using s[j]
+            j += 1 # shrink the left edge while invalid
+        ans = max(ans, i - j + 1) # longest window so far
+    return ans # longest window
+
+
+
+""input1 = ["abcabcbb", "bbbbb", "pwwkew"]
+for input in input1:
+    print("lengthOfLongestSubstringWithoutRepeating(\"{}\") = {}".format(
+        input,
+        lengthOfLongestSubstringWithoutRepeating(input)))
+"""print()    
+input2 = ["eceba", "aa"]
+for input in input2:
+    for k in range(1, len(input) + 1):
+        print("lengthOfLongestSubstringKDistinct(\"{}\", {}) = {}".format(
+            input,
+            k,
+            lengthOfLongestSubstringKDistinct(input, k)))
+
+"""""
